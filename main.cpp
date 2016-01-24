d4e10a2980446d6500618286621ad41bc7f61043
girldrummer <b.j.williamsstewart@gmail.com>
2016-01-24 14:05:14 -0900

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Capstone
+//
+//  Created by BRIANA on 1/24/16.
+//  Copyright © 2016 BRIANA. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
